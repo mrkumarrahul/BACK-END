19273d18a38a636b3bc875378b37a0bf2bf45562
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-11-16 18:12:40 +0530

    handling post request

--- Day33/BackEnd/index.js
+++ Day33/BackEnd/index.js
@@ -1,15 +1,17 @@
 const express=require("express");
 const app=express()
 const port=3000;
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.get("/register",(req,res) => {
   let {user,password}=req.query;
   res.send(`standard GET response. Welcome ${user}!`);
 })
 
 app.post("/register",(req,res) =>{
-  
-  res.send("Standard POST response.")
-})
+  let {user,password}= req.body;
+  res.send(`Welcome To Post Response.Welcome to ${user}`);
+});
 app.listen(port, () => {
   console.log(`listening to port ${port} `);
 })
